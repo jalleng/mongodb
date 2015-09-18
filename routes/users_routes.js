a53e78238ab9ac50c9c8ee4352b77d47c1386370
Jeff Gerber <jallengerber@gmail.com>
2015-09-18 16:59:27 -0700

    depyramidification of users post route

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -5,6 +5,8 @@ var User = require(__dirname + '/../models/user');
 var jsonParser = require('body-parser').json();
 var handleError = require(__dirname + '/../lib/handle_error');
 var httpBasic = require(__dirname + '/../lib/http_basic');
+var EE = require('events').EventEmitter;
+var routeEvents = new EE();
 
 var usersRouter = module.exports = exports = express.Router();
 
@@ -13,38 +15,41 @@ usersRouter.post('/signup', jsonParser, function(req, res) {
 
   newUser.basic.username = req.body.username;
   newUser.username = req.body.username;
-
+  console.log("username", req.body.username)
   newUser.generateHash(req.body.password, function(err, hash) {
+    if (err) return res.send("wat?")
+      routeEvents.emit("hash", hash);
+  });
 
-    if (err) return res.send("hey")
+  routeEvents.on("hash", function(hash) {
     newUser.save(function(err, data) {
-
       if (err) return res.send("this broke")
-      newUser.generateToken(function(err, token) {
+        routeEvents.emit("save", data);
+    });
+  });
 
-        if (err) return res.send("lulwat")
+  routeEvents.on("save", function(data) {
+    newUser.generateToken(function(err, token) {
+      if (err) return res.send("lulwat")
         res.json({token: token});
-      });
     });
   });
 });
+
+
 
 usersRouter.get('/signin', httpBasic, function(req, res) {
 
   User.findOne({'basic.username': req.auth.username}, function(err, user) {
 
     if (err) return handleError(err, res);
-
-
     if (!user) {
-
       return res.status(401).json({msg: 'Meow1! Could not authenticat!'});
     }
 
     user.compareHash(req.auth.password, function(err, hashRes) {
       if (err) return handleError(err, res);
       if (!hashRes) {
-
         return res.status(401).json({msg: 'Meow2! Could not authenticat!'});
       }
 
